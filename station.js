6ea1c105ca4c5d43c1a8b4c65020139b0b7105d9
iki1uc <entertrainer@web.de>
2026-07-30 18:48:56 +0200

    Update station.js

--- station.js
+++ station.js
@@ -1,1 +1,13 @@
+import { Station } from "../station.js";
+import form from "./form/f.md";
+import struktur from "./struktur/s.md";
+import verhalten from "./verhalten/v.md";
+import präsenz from "./präsenz/p.md";
+
+export const AGENT_STATION = Station("AGENT", {
+    form,
+    struktur,
+    verhalten,
+    präsenz
+});
 
